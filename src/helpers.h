902cfcf06ce4417e4426c027cfb87a3060d7e74f
arunchavan89 <arunchavan89@gmail.com>
2020-04-24 14:41:40 +0200

    Adding first draft of spline

--- src/helpers.h
+++ src/helpers.h
@@ -30,7 +30,7 @@ string hasData(string s) {
 //
 
 // For converting back and forth between radians and degrees.
-constexpr double pi() { return M_PI; }
+constexpr double pi() { return 3.141592; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
